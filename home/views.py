019179d5775ee0cc08fa955fed58ad02cc84c68a
Nazmul Hasan <nazmul.buet17@gmail.com>
2021-04-10 04:20:01 +0600

    upto Form

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from .models import Slider , Services
 def home(request):
 	sliderData = Slider.objects.all()
 	serviceData = Services.objects.all()
-	return render(request,'home/homebase.html',{'slider':sliderData,'services':serviceData})
+	return render(request,'home/base.html',{'slider':sliderData,'services':serviceData})
